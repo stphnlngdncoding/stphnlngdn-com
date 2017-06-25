055d8af521b22b29e3b5951a67c49b98028473c4
Stephen Langdon <stphnlngdncoding@gmail.com>
2017-06-25 01:29:00 -0700

    changed static path

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 })
 
 app.listen(9000, () => console.log('server started on port 9000'));
